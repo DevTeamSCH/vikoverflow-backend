996b1dbe9195cad6700fa16916e52ae78986c8ee
Barnabás Czémán <trabarni@gmail.com>
2018-10-24 20:09:26 +0200

    Add ticket endpoint.

---
+++ src/feedback/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from . import models
+
+
+class TicketSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.Ticket
+        fields = ('url', 'title', 'text', 'kind_of')
--- src/feedback/views.py
+++ src/feedback/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import mixins
+from rest_framework import permissions
 
-# Create your views here.
+from common.mixins import RelativeURLFieldMixin
+from . import models
+from . import serializers
+
+
+class TicketViewSet(
+    RelativeURLFieldMixin,
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = models.Ticket.objects.all()
+    serializer_class = serializers.TicketSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def check_permissions(self, request):
+        # Allow GET when ticket was posted
+        if request.method in ["DELETE", "GET"] and request.POST.get('pk') is None:
+            self.permission_classes.append(permissions.IsAdminUser)
+        super().check_permissions(request)
