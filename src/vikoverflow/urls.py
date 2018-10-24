3cc04f0086bed13352d4a1a59a95ad3bdaa551ce
Barnabás Czémán <trabarni@gmail.com>
2018-10-24 20:09:26 +0200

    Add ticket endpoint.

--- src/vikoverflow/urls.py
+++ src/vikoverflow/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api/v1/', include('question.urls')),
     path('api/v1/', include('account.urls')),
-    path('api/v1/', include('social_django.urls', namespace='social')),
     path('api/v1/', include('moderate.urls')),
     path('api/v1/', include('feedback.urls')),
+    path('api/v1/', include('social_django.urls', namespace='social')),
+
+    # Server Side Rendering
+    path('admin/', admin.site.urls),
 ]
