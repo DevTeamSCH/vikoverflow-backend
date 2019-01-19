d212bb1533ce26dabd35971ff2241dd9198c48b5
Daen <g.dani325@gmail.com>
2019-01-19 20:56:23 +0100

    Validation requested change

--- src/question/serializers.py
+++ src/question/serializers.py
@@ -8,6 +8,17 @@ from account.models import Profile
 
 
 class CommentSerializer(AbstractCommentSerializer):
+    def validate(self, data):
+        if data['parent_answer'] is None and data['parent_question'] is None:
+            raise ValidationError(
+                'Comment must have a parent answer or question!'
+            )
+        if data['parent_answer'] is not None and data['parent_question'] is not None:
+            raise ValidationError(
+                'Comment must only have one parent!'
+            )
+        return data
+
     class Meta:
         model = models.Comment
         fields = (
