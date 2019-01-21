30c3788b4f8e4e9566a55cae1810df2a5d55ee81
Márki-Zay Ferenc <ferencmarkizay@gmail.com>
2019-01-22 00:04:54 +0100

    Fix permission logic

--- src/moderate/permissions.py
+++ src/moderate/permissions.py
@@ -1,10 +1,12 @@
 from rest_framework.permissions import BasePermission
 
 
+# Permission class for the report viewset. All authenticated users are allowed to create reports, but only staff can
+# access the other actions.
 class ReportViewSetPermission(BasePermission):
     def has_permission(self, request, view):
         if not request.user:
             return False
-        if request.method == "POST":
+        if request.method == "POST" and view.action == "create":
             return True
         return request.user.is_staff
