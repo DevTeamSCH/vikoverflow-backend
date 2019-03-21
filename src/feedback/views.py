57dd6bde135f22c6c9186def4d187f4800d799a4
Barnabás Czémán <barni2000@users.noreply.github.com>
2019-03-21 13:33:54 +0100

    Add feedback endpoints.
    Now endpoints for user issues are available.
    
    Issue: DevTeamSCH/vikoverflow#6

--- src/feedback/views.py
+++ src/feedback/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import mixins
+from rest_framework import permissions
 
-# Create your views here.
+from common.mixins import RelativeURLFieldMixin
+from .permissions import IsAdminOrCreate
+from . import models
+from . import serializers
+
+
+class TicketViewSet(
+    RelativeURLFieldMixin,
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = models.Ticket.objects.all()
+    serializer_class = serializers.TicketSerializer
+    permission_classes = (permissions.IsAuthenticated, IsAdminOrCreate)
