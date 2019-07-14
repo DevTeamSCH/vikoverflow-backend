c13570bcfdaaa88d403da5a34ca70bee5ef2442d
Daniel Gal <daniel.gal@sch.bme.hu>
2019-07-15 00:23:06 +0200

    Fix for flake8

--- src/question/views.py
+++ src/question/views.py
@@ -1,5 +1,3 @@
-import json
-
 from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
 from django.shortcuts import get_object_or_404
 from rest_framework import mixins
@@ -7,7 +5,6 @@ from rest_framework import permissions
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.decorators import action
-from rest_framework.response import Response
 
 from account.models import Profile
 from common.models import Votes
