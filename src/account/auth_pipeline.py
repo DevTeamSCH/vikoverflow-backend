5985870e5fef3c5708d3d476adbdb721cb268303
Daniel Gal <daniel.gal@sch.bme.hu>
2020-02-10 12:08:52 +0100

    Display name set before create user

--- src/account/auth_pipeline.py
+++ src/account/auth_pipeline.py
@@ -6,12 +6,20 @@ from . import models
 
 
 @partial
+def set_display_name(strategy, backend, is_new=False, *args, **kwargs):
+    if not backend.name == "authsch" and not is_new:
+        return
+    display_name = strategy.session_get("displayName", None)
+    if not display_name:
+        return redirect("/first-login")
+
+
 def create_profile(strategy, backend, request, details, user, *args, **kwargs):
-    if backend.name == "authsch":
-        try:
-            user.profile
-        except exceptions.ObjectDoesNotExist:
-            display_name = strategy.session_get("displayName", None)
-            if not display_name:
-                return redirect("/first-login")
-            models.Profile.objects.create(user=user, display_name=display_name)
+    if not backend.name == "authsch":
+        return
+    try:
+        user.profile
+    except exceptions.ObjectDoesNotExist:
+        display_name = strategy.session_get("displayName", None)
+        models.Profile.objects.create(user=user, display_name=display_name)
+
