e914b903a69b19f652aafd72735a9540ad2eed28
Daniel Gal <daniel.gal@sch.bme.hu>
2020-02-10 12:18:47 +0100

    Fixed & refactored guard conditions

--- src/account/auth_pipeline.py
+++ src/account/auth_pipeline.py
@@ -7,19 +7,17 @@ from . import models
 
 @partial
 def set_display_name(strategy, backend, is_new=False, *args, **kwargs):
-    if not backend.name == "authsch" and not is_new:
-        return
-    display_name = strategy.session_get("displayName", None)
-    if not display_name:
-        return redirect("/first-login")
+    if backend.name == "authsch" and is_new:
+        display_name = strategy.session_get("displayName", None)
+        if not display_name:
+            return redirect("/first-login")
 
 
 def create_profile(strategy, backend, request, details, user, *args, **kwargs):
-    if not backend.name == "authsch":
-        return
-    try:
-        user.profile
-    except exceptions.ObjectDoesNotExist:
-        display_name = strategy.session_get("displayName", None)
-        models.Profile.objects.create(user=user, display_name=display_name)
+    if backend.name == "authsch":
+        try:
+            user.profile
+        except exceptions.ObjectDoesNotExist:
+            display_name = strategy.session_get("displayName", None)
+            models.Profile.objects.create(user=user, display_name=display_name)
 
